ff36bb498ef4f06180feee312ea1303a65da1065
StaryFritz <mihail.tennis@gmail.com>
2022-12-05 18:16:40 +0300

    commit

---
+++ src/test/java/by/lahunouski/javaproject/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lahunouski.javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
